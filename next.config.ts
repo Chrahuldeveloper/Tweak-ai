246858f75ba56a3d74a64ca094bea6c5a376586d
Chrahuldeveloper <chrahulofficial@gmail.com>
2025-10-18 15:14:35 +0530

    extension config dn

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  images: { unoptimized: true },
+  distDir: "build",
 };
 
 export default nextConfig;
